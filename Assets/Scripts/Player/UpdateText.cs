4ee6ed74fe39f14b4e0bcdc98cc98f0791cd5e5c
sjkaahf <rain782260@gmail.com>
2025-07-19 15:28:24 +0700

    Update chạy nhanh
    Sửa và thêm 1 số biến ở script playerController, sửa script UpdateText bằng cách thêm biến và thêm hàm UpdateStamina

--- Assets/Scripts/Player/UpdateText.cs
+++ Assets/Scripts/Player/UpdateText.cs
@@ -5,10 +5,27 @@ public class UpdateText : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private TextMeshProUGUI scoreView;
     private TextMeshProUGUI levelView;
+
+    private TextMeshProUGUI staminaView;
+
+    public static UpdateText Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;  // Assign the singleton
+        }
+        else
+        {
+            Destroy(gameObject);  // Prevent duplicates
+        }
+    }
     void Start()
     {
         scoreView = GameObject.Find("ScoreView").GetComponent<TextMeshProUGUI>();
         levelView = GameObject.Find("LevelView").GetComponent<TextMeshProUGUI>();
+        staminaView = GameObject.Find("StaminaView").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -17,4 +34,9 @@ public class UpdateText : MonoBehaviour
         scoreView.text = $"Score: {ScoreManager.Instance.GetScore()}";
         levelView.text = $"Level: {ScoreManager.Instance.GetCurrentLevel()}";
     }
-}
+    public void UpdateStamina(float stamina)
+    {
+        staminaView.text = $"Stamina: {stamina:F1}"; // Display stamina with one decimal place
+    }
+}   
+
