089e6132b2746b8339d905a06ee771b1e893ff6a
sjkaahf <rain782260@gmail.com>
2025-07-20 13:44:57 +0700

    Update màn hình, biến chạy
    - Màn hình thành dọc
    - Cho hiện lên các biến stamina

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
-
+// encapsulation, modularization, 
+// con gà: biết bay, script nhảy cao, script đi theo ng chơi
+// vịt: biết bơi, script bơi dưới nước, script đi theo ng chơi
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float normalMoveSpeed = 5f;
     [SerializeField] private float fasterMoveSpeed = 10f;
-    private float stamina = 100f; // Example stamina variable
-    private float staminaCostPerSecond = 10f; // Example stamina cost per second when sprinting
-    private float staminaRegeneratedPerSecond = 5f; // Example stamina regenerated per second when not sprinting
+    [SerializeField] private float stamina = 100f; // Example stamina variable
+    [SerializeField] private float staminaCostPerSecond = 30f; // Example stamina cost per second when sprinting
+    [SerializeField] private float staminaRegeneratedPerSecond = 10f; // Example stamina regenerated per second when not sprinting
     private float moveSpeed;
     private Rigidbody2D rb;
+
 
     private void Awake()
     {
@@ -18,7 +21,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update() // update stamina, move, UI 
     {
-        if (Input.GetKey(KeyCode.Space) && stamina > 0)
+        RunFast();
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 input = InputManager.Instance.moveDirection;
+        rb.linearVelocity = input * moveSpeed;
+    }
+
+    public void RunFast()
+    {
+        // This method can be used to trigger fast running logic if needed
+        // Currently, it does nothing but can be expanded later
+        if (Input.GetKey(KeyCode.Space) && stamina >= staminaCostPerSecond)
         {
             //check if player is moving
             if (rb.linearVelocity.magnitude > 0)
@@ -30,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
             if (stamina < 0) stamina = 0; // Prevent negative stamina
             moveSpeed = fasterMoveSpeed;
         }
-        else if (UpdateText.Instance.isSprintButtonPressed) //check if ui button is pressed
+        else if (UpdateText.Instance.isSprintButtonPressed && stamina >= staminaCostPerSecond) //check if ui button is pressed
         {
             // You can handle sprint logic here if needed
             // For example, set moveSpeed = fasterMoveSpeed;
@@ -54,13 +71,6 @@ public class PlayerMovement : MonoBehaviour
                 if (stamina > 100f) stamina = 100f; // Cap stamina at 100
             }
         }
-        Move();
-    }
-
-    private void Move()
-    {
-        Vector2 input = InputManager.Instance.moveDirection;
-        rb.linearVelocity = input * moveSpeed;
     }
 
 }
